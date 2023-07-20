5e85b963db081cbe65f31e2214abf7e77d6e90d4
Sajer24 <sajer.kanat1992@gmail.com>
2023-07-20 20:55:29 +0300

    finally

--- src/Main.groovy
+++ src/Main.groovy
@@ -1,3 +1,13 @@
 static void main(String[] args) {
-  println "Hello world!"
+    def myList = [1, 2, 3, 2, 1, 3, 4, 5, 4, 1, 4, 3, 3]
+    def result = countElements(myList)
+    println(result)
+}
+
+static def countElements(list) {
+    def resultMap = [:]
+    list.each { element ->
+        resultMap[element] = resultMap.getOrDefault(element, 0) + 1
+    }
+    return resultMap
 }
